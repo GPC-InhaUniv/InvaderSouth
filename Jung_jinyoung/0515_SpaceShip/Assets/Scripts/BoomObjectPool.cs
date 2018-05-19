038e210e58ea5a5616b76e3e0fff09ca1c914703
정진영 <qufqlaud@gmail.com>
2018-05-19 16:40:05 +0900

    0519_Jung_jinyoung_PlayerMove,Attack,Missile,skill

---
+++ Jung_jinyoung/0515_SpaceShip/Assets/Scripts/BoomObjectPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomObjectPool : MonoBehaviour {
+
+    public GameObject Boom;
+    public int BoomCount = 30;
+    List<GameObject> Booms;
+    public float spaqwnWait;
+    public float startWait;
+    public float waveWait;
+    public Vector3 spawnValue;
+    Vector3 spawnPosition;
+    Quaternion spawnRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //object pool 생성
+        Booms = new List<GameObject>();
+        for (int i = 0; i < BoomCount; i++)
+        {
+            GameObject obj = Instantiate(Boom);
+            obj.SetActive(false);
+            Booms.Add(obj);
+        }
+        //Object Pool 생성 끝
+        // StartCoroutine(SpawnWaves());
+    }
+
+    //IEnumerator SpawnWaves()
+    //{
+    //    yield return new WaitForSeconds(startWait);
+    //    foreach(GameObject boom in Booms)
+    //    {
+    //        spawnPosition = new Vector3()
+    //    }
+    //    while (true)
+    //    {
+    //        for (int i = 0; i < Booms.Count; i++)
+    //        {
+    //            spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+    //            spawnRotation = Quaternion.identity;
+
+    //            Booms[i].SetActive(true);
+    //            Booms[i].transform.position = spawnPosition;
+    //            Booms[i].transform.rotation = spawnRotation;
+    //            yield return new WaitForSeconds(spaqwnWait);
+    //        }
+    //        yield return new WaitForSeconds(waveWait);
+    //    }
+    //}
+}
---
+++ Jung_jinyoung/0515_SpaceShip/Assets/Scripts/DestroyByContect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContect : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject plyerExplosion;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        
+        if(other.tag == "Player")
+        {
+            Instantiate(plyerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
