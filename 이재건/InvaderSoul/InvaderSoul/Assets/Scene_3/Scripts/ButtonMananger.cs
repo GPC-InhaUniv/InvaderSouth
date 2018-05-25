08a4bde06f8d934ced8a5a32cb8942d7cc38d5ab
이재건 <jaegun92@gmail.com>
2018-05-25 15:17:45 +0900

    PIST-19 게임 로딩씬, 적 생성 패턴 구상 및 설계

---
+++ 이재건/InvaderSoul/InvaderSoul/Assets/Scene_3/Scripts/ButtonMananger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMananger : MonoBehaviour {
+
+   
+
+    private void Awake()
+    {
+        Screen.SetResolution(700, 1080, true);
+  
+    }
+
+
+    public void OnClickLogin()
+    {
+        LoadingSceneController.LoadScene("Main");
+    }
+}
---
+++ 이재건/InvaderSoul/InvaderSoul/Assets/Scene_3/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public BulletPool PlayerBulletPool;
+
+    // Use this for initialization
+	void Start ()
+    {
+        PlayerBulletPool = GameObject.Find("GameObjectPool").GetComponent<BulletPool>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + 0.2f, 3.6f, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - 0.2f, 3.6f, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerBulletPool.Fire(this.transform);
+        }
+
+    }
+}
