9d34e4d400cd5f69a9ed17c99f7abf896919a656
이재건 <jaegun92@gmail.com>
2018-05-29 01:09:25 +0900

    PlayFabTest

---
+++ 이재건/InvaderSoul/Assets/PlayFabScripts/DataManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PlayFab.ClientModels;
+using PlayFab;
+
+public class DataManager : MonoBehaviour
+{
+
+    public AccountInfo Info;
+
+    public static DataManager Instance;
+
+    [SerializeField]
+    private List<GameObject> menus;
+
+
+    private void Awake()
+    {
+        if (Instance != this)
+            Instance = this;
+
+        Info = GameObject.Find("AccountInfo").GetComponent<AccountInfo>();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void ChangeMenu(int i)
+    {
+        GameFunctions.ChangeMenu(menus.ToArray(), i);
+    }
+
+
+    public void UseMoney()
+    {
+        UserDataRecord userData = new UserDataRecord();
+        Info.Info.UserData.TryGetValue("PlayerMoney", out userData);
+        userData.Value = "90";
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data.Add("PlayerMoney", userData.Value);
+        data.Add("PlayerStage", "0");
+
+        UpdateUserDataRequest request = new UpdateUserDataRequest()
+        {
+            Data = data,
+
+        };
+        PlayFabClientAPI.UpdateUserData(request, UpdateDataInfo, GameFunctions.OnAPIError);
+    }
+    void UpdateDataInfo(UpdateUserDataResult result)
+    {
+        Debug.Log("UpdateDataInfo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Info != null)
+        //    name.text = Info.Info.AccountInfo.Username;
+
+        //UserDataRecord recod = new UserDataRecord();
+        //if (Info.Info.UserData != null)
+        //{
+        //    if (Info.Info.UserData.TryGetValue("PlayerMoney", out recod))
+        //    {
+                
+        //        money.text = recod.Value;
+              
+        //    }
+        //}
+
+
+
+    }
+}
---
+++ 이재건/InvaderSoul/Assets/Scene_1/Scripts/TitleManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("LoginPanel")]
+    public InputField IDInputField;
+    public InputField PassInputField;
+
+    [Header("CreateAccountPanel")]
+    public InputField New_IDInputField;
+    public InputField New_PassInputField;
+    public InputField Confirm_New_PassInputField;
+
+    [Header("PopUp")]
+    public GameObject LoginAlertPanel;
+    public GameObject CreateAlertPanel;
+    public GameObject CreateAccountPanel;
+    public GameObject LoginPanel;
+    public Text CreateAccountAlert;
+
+    string playerID = "";
+    string secreatNum = "";
+    private void Start()
+    {
+        Screen.SetResolution(700, 1080, true);
+    }
+    /// <summary>
+    /// Login, CreateAccount
+    /// </summary>
+    public void LoginBtn()
+    {
+        StartCoroutine(LginCo());
+    }
+
+    IEnumerator LginCo()
+    {
+        if (IDInputField.text != "" && PassInputField.text != "")
+        {
+            //아이디 및 비밀번호를 임시 변수에 저장
+            playerID = IDInputField.text;
+            secreatNum = PassInputField.text;
+
+            Debug.Log("ID는?" + IDInputField.text);
+            Debug.Log("비밀번호는?" + PassInputField.text);
+
+            if (!LoginManager.isError)
+            {
+               
+                Debug.Log("로그인 성공");
+            }
+            else
+            {
+                CreateAccountAlert.text = "아이디 및 비밀번호 오류입니다.";
+                CreateAccountAlert.gameObject.SetActive(true);
+                Debug.Log("로그인 실패");
+            }
+
+        }
+        yield return null;
+    }
+
+    public void OpenCreateAccountBtn()
+    {
+        CreateAccountPanel.SetActive(true);
+    }
+
+    public void CreateAccountBtn()
+    {
+        StartCoroutine(CreateCo());
+    }
+
+    IEnumerator CreateCo()
+    {
+        playerID = New_IDInputField.text;
+        secreatNum = New_PassInputField.text;
+
+
+
+        if (!LoginManager.isError)
+        {
+            CreateAccountAlert.text = "계정 생성 성공! 로그인 해주세요!";
+            CreateAccountAlert.gameObject.SetActive(true);
+            Debug.Log("계정 생성 성공");
+        }
+        else
+        {
+            Debug.Log("계정 생성 실패");
+        }
+
+
+        yield return null;
+    }
+
+    public void ShowLoginAlertPanel()
+    {
+        if (IDInputField.text == "" || PassInputField.text == "")
+        {
+            LoginAlertPanel.gameObject.SetActive(true);
+        }
+    }
+
+    public void HideLoginAlertPanel()
+    {
+        LoginAlertPanel.gameObject.SetActive(false);
+    }
+
+    public void ShowCreateAlertPanel()
+    {
+        CreateAlertPanel.gameObject.SetActive(true);
+
+    }
+    public void HideCreateAlertPanel()
+    {
+        CreateAlertPanel.gameObject.SetActive(false);
+    }
+
+    public void HideCreateAccountPanel()
+    {
+        CreateAccountPanel.gameObject.SetActive(false);
+    }
+
+    public void HideLoginPanel()
+    {
+        LoginPanel.gameObject.SetActive(false);
+    }
+
+    public void ShowLoginPanel()
+    {
+        LoginPanel.gameObject.SetActive(true);
+    }
+
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
