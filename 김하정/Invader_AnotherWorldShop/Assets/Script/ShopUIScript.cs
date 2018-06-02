ff4d08e9d282d7eb6614311c46ba06f32e6593ee
김하정 <hajeongkim1120@gmail.com>
2018-06-02 17:49:56 +0900

    PIST-41 김하정 상점

---
+++ 김하정/Invader_AnotherWorldShop/Assets/Script/ShopUIScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ShopUIScript : MonoBehaviour 
+{
+
+    public GameObject ShopPanel;
+  
+     
+    public GameObject ItemInfoPanel;
+
+    public List<Toggle> ShopToggleList;
+
+    public List<Text> ItemPrice;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetToggleOff();
+
+    }
+
+    void ResetToggleOff()
+    {
+        for (int i = 0; i < ShopToggleList.Count; i++)
+        {
+            ShopToggleList[i].isOn = false;
+        }
+    }
+
+     
+
+
+
+    public void GetItemPrice()
+    {
+        for (int i = 0; i < ShopToggleList.Count; i++)
+        {
+            if (ShopToggleList[i].isOn)
+            {
+                Debug.Log(ShopToggleList[i]+"는 토글 켜졌음");
+            }
+            else 
+            {
+                Debug.Log(ShopToggleList[i] + "는 토글 꺼져있음");
+            }
+        }
+    }
+
+
+    public void CloseShopBtn()
+    {
+        ShopPanel.SetActive(false);
+    }
+
+    public void Show()
+    {
+        
+        ItemInfoPanel.SetActive(true);
+        
+    }
+
+    public void Hide()
+    {
+         
+        ItemInfoPanel.SetActive(false);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
