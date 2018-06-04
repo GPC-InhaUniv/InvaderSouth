91d76f7f5f61e215529569e60c951d9472ab1c27
이재건 <jaegun92@gmail.com>
2018-06-04 14:32:57 +0900

    PIST-49 타이틀 화면과 로비화면을 연결하여 작동하도록 작업

---
+++ 이재건/InvaderAnotherWorld/Assets/Scene_1/Scripts/LobbyUIScript.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUIScript : MonoBehaviour
+{
+    // UI 위치가 어딘지 몰라서 Diamond UI는 아직 안넣었습니다.
+    private int playerDiamondCount;
+    // lastCompletedStageNumber = GameManager.lastCompletedStageNumber;
+    private int lastCompletedStageNumber = 3;
+    // planeCount = GameManager.planeCount;
+    private int planeCount = 1;
+
+    private GameObject characterSelectionUIPanel;
+    private GameObject stageSelectionUIPanel;
+    private GameObject firstCharacterInfoImage;
+    private GameObject secondCharacterInfoImage;
+    private GameObject firstCharacter;
+    private GameObject secondCharacter;
+    private GameObject shopPanel;
+ 
+
+    private Button stageOneSelectionButton;
+    private Button stageTwoSelectionButton;
+    private Button stageThreeSelectionButton;
+    private Text playerMoneyCountText;
+    private Text playerNameText;
+
+    private void Awake()
+    {
+        characterSelectionUIPanel = GameObject.Find("LobbyUICanvas").transform.Find("CharacterSelectionUIPanel").gameObject;
+        stageSelectionUIPanel = GameObject.Find("LobbyUICanvas").transform.Find("StageSelectionUIPanel").gameObject;
+        shopPanel = GameObject.Find("LobbyUICanvas").transform.Find("ShopPanel").gameObject;
+        firstCharacterInfoImage = characterSelectionUIPanel.transform.Find("FirstCharacterInfoImage").gameObject;
+        secondCharacterInfoImage = characterSelectionUIPanel.transform.Find("SecondCharacterInfoImage").gameObject;
+        firstCharacter = characterSelectionUIPanel.transform.Find("FirstCharacter").gameObject;
+        secondCharacter = characterSelectionUIPanel.transform.Find("SecondCharacter").gameObject;
+        stageOneSelectionButton = stageSelectionUIPanel.transform.Find("StageOneSelectionButton").GetComponent<Button>();
+        stageTwoSelectionButton = stageSelectionUIPanel.transform.Find("StageTwoSelectionButton").GetComponent<Button>();
+        stageThreeSelectionButton = stageSelectionUIPanel.transform.Find("StageThreeSelectionButton").GetComponent<Button>();
+        playerMoneyCountText = characterSelectionUIPanel.transform.Find("PlayerNameAndMoneyImage").transform.Find("PlayerMoneyCountText").GetComponent<Text>();
+        playerNameText = characterSelectionUIPanel.transform.Find("PlayerNameAndMoneyImage").transform.Find("PlayerNameText").GetComponent<Text>();
+
+        lastCompletedStageNumber = Convert.ToInt32(GameManager.Instance.GetPlayerLastCompletedStageNumber());
+
+        // playerMoneyCountText.text = GameManager.GetPlayerMoney();
+        // playerNameText.text = GameManager.GetPlayerName();
+        // lastCompletedStageNumber = GameManager.LastCompletedStageNumber;
+        // playerDiamondCount = GameManager.GetPlayerDiamond();
+    }
+
+    private void Update()
+    {
+        playerMoneyCountText.text = GameManager.Instance.GetPlayerMoney();
+        playerNameText.text = GameManager.Instance.GetPlayerName();
+    }
+
+    public void OnClickStartButtonOfCharacterSelection()
+    {
+        characterSelectionUIPanel.SetActive(false);
+        stageSelectionUIPanel.SetActive(true);
+        lastCompletedStageNumber =Convert.ToInt32(GameManager.Instance.GetPlayerLastCompletedStageNumber());
+        CheckTheDifficulty();
+        Debug.Log("Start Button Click!");
+    }
+
+    public void OnClickRightArrowButtonOfCharacterSelection()
+    {
+        firstCharacter.SetActive(false);
+        firstCharacterInfoImage.SetActive(false);
+        secondCharacter.SetActive(true);
+        secondCharacterInfoImage.SetActive(true);
+        Debug.Log("Right Arrow Button Click!");
+    }
+
+    public void OnClickLeftArrowButtonOfCharacterSelection()
+    {
+        firstCharacter.SetActive(true);
+        firstCharacterInfoImage.SetActive(true);
+        secondCharacter.SetActive(false);
+        secondCharacterInfoImage.SetActive(false);
+        Debug.Log("Left Arrow Button Click!");
+    }
+
+    public void OnClickBackwardMovementButtonOfStageSelection()
+    {
+        characterSelectionUIPanel.SetActive(true);
+        stageSelectionUIPanel.SetActive(false);
+        Debug.Log("Backward Movement Button Click!");
+    }
+
+    public void OnClickStageButtonClick()
+    {
+        Debug.Log("Stage Button Click");
+        Debug.Log("Go to the store");
+        shopPanel.SetActive(true);
+
+
+    }
+
+    private void CheckTheDifficulty()
+    {
+        if (lastCompletedStageNumber >= 2)
+            stageTwoSelectionButton.interactable = true;
+        if (lastCompletedStageNumber == 3)
+            stageThreeSelectionButton.interactable = true;
+    }
+}
---
+++ 이재건/InvaderAnotherWorld/Assets/Scene_1/Scripts/ShopUIScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ShopUIScript : MonoBehaviour 
+{
+    public GameObject ShopPanel;
+    public List<Toggle> ShopToggleList;
+    public List<Text> ItemPriceText;
+    
+    //private int playerMoneyCountText =  GameManager.GetPlayerMoney();
+    private int playerDiamondCount = 50;
+    //private int playerDiamondCount =  GameManager.GetPlayerDiamond();
+    public Text ShowTotalItemPrice;
+    public Text CurrentPlayerCash;
+
+    int totalItemPrice = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetToggleOff();
+        GetItemPrice();
+
+    }
+    private void Update()
+    {
+        CurrentPlayerCash.text = GameManager.Instance.GetPlayerMoney() + "G";
+    }
+
+    void ResetToggleOff()
+    {
+        for (int i = 0; i < ShopToggleList.Count; i++)
+        {
+            ShopToggleList[i].isOn = false;
+        }
+    }
+
+
+    public void GetItemPrice()
+    {
+        int itemprice = 0;
+        for (int i = 0; i < ShopToggleList.Count; i++)
+        {
+           
+            if (ShopToggleList[i].isOn)
+            {
+                itemprice += Convert.ToInt32(ItemPriceText[i].text);
+                Debug.Log(ShopToggleList[i] + "는 토글 켜져있음-ON");
+            }
+            else 
+            {
+                Debug.Log(ShopToggleList[i] + "는 토글 꺼져있음-OFF");
+            }
+            
+        }
+        Debug.Log(itemprice);
+        if (itemprice == 0)
+        { ShowTotalItemPrice.text = itemprice.ToString();   }
+        else
+        { ShowTotalItemPrice.text = "-" + itemprice.ToString(); }
+
+
+
+    }
+
+ 
+    public void CloseShopBtn()
+    {
+        ShopPanel.SetActive(false);
+    }
+
+    public void OnClickStartBtn()
+    {
+        totalItemPrice = Convert.ToInt32(ShowTotalItemPrice.text);
+        int calculateMoneyBeforeStart = Convert.ToInt32(GameManager.Instance.GetPlayerMoney()) + totalItemPrice;
+        if (calculateMoneyBeforeStart < 0)
+            Debug.Log("소지 금액 부족");
+        else
+        {
+            DataManager.Instance.BuyItem(totalItemPrice);
+            LoadingSceneController.LoadScene("Main");
+        }
+    }
+
+
+}
