b02580ddaa9d55c3c05a82079c1d26a4cf86d30c
김하정 <hajeongkim1120@gmail.com>
2018-06-09 14:18:18 +0900

    PIST-55 김하정 결과창 완료

--- 김하정/AnotherWorld_InGame/AnotherWorld_InGame/Assets/Scripts/GameResultScript.cs
+++ 김하정/AnotherWorld_InGame/AnotherWorld_InGame/Assets/Scripts/GameResultScript.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameResultScript : MonoBehaviour {
 
     private Text getPlayerScoreText;
     private Text getPlayerGoldText;
-    private int playerScore = 500;
+    private int playerScore = 50;  //이번 게임에서 플레이어가 획득한 점수 //임시 값으로 넣어 놨음
     private int tempScore = 0;
     private PlayerController playerstate;
     private GameObject gameEndResultPanel;
-    private int playerGold = 100;
+    private int playerGold = 100;   //이번 게임에서 플레이어가 획득한 골드. //임시 값으로 넣어 놨음
     private int tempGold = 0;
-    private bool coroutineflag=true;
+    private bool coroutineFlag=true;
+    private  Button nextButton;
+    private int playerMoney;
+    private int changeScoreBuffer;
 
 
     void Start ()
@@ -22,29 +26,35 @@ public class GameResultScript : MonoBehaviour {
         getPlayerGoldText = GameObject.Find("GetPlayerGoldText").GetComponent<Text>();
         playerstate = GameObject.Find("PlayerShip").GetComponent<PlayerController>();
         gameEndResultPanel = GameObject.Find("GameEndResultPanel");
+       
+        nextButton = GameObject.Find("NextButton").GetComponent<Button>();
+        nextButton.gameObject.SetActive(false);
         gameEndResultPanel.gameObject.SetActive(false);
+
+        changeScoreBuffer = playerScore - 15;
+
+
 
     }
 
     private void Update()
     {
-        if (playerstate.isGameOver&& coroutineflag)
+        if (playerstate.isGameOver&& coroutineFlag)
         {
             gameEndResultPanel.gameObject.SetActive(true);
             StartCoroutine("ChangeScoreCoroutine");
             StartCoroutine("ChangeGoldCoroutine");
-            coroutineflag = false;
+            coroutineFlag = false;
         }
 
-        else if (playerstate.isGameClear && coroutineflag)
+        else if (playerstate.isGameClear && coroutineFlag)
         {
             gameEndResultPanel.gameObject.SetActive(true);//게임 클리어 화면 트루로 바꿔주기
+            nextButton.gameObject.SetActive(true);
             StartCoroutine("ChangeScoreCoroutine");
             StartCoroutine("ChangeGoldCoroutine");
-            coroutineflag = false;
+            coroutineFlag = false;
         }
-
-
 
     }
 
@@ -57,8 +67,13 @@ public class GameResultScript : MonoBehaviour {
         {
             tempScore += 1;
             getPlayerScoreText.text = string.Format("{0:D3}", tempScore);
-                
-            yield return new WaitForSeconds(0.01f);
+            if (tempScore < changeScoreBuffer) 
+            {
+                yield return new WaitForSeconds(0.01f);
+            }
+            else if ( changeScoreBuffer < tempScore  ) 
+            { yield return new WaitForSeconds(0.1f); }
+             
         }
         yield return null; 
     }
@@ -79,7 +94,13 @@ public class GameResultScript : MonoBehaviour {
         yield return null;
     }
 
-
+    public  void RestartBtn()
+    {
+        playerGold = 0; //어차피 게임내에서 획득한 점수/골드만 초기화 하면 되는것 아닌가여?0ㅇ0
+        playerScore = 0;
+        SceneManager.LoadScene("  ");   //씬 이름 넣어줄것.
+          //아이템은 게임 매니저에 구현되면 넣을 예정.   
+    }
 
 
 
