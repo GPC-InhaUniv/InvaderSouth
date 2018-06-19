7d2ece9a13932a27e2085ce6dab7c3a73df35ef6
정진영 <qufqlaud@gmail.com>
2018-06-19 13:28:29 +0900

    0619_메인에 합치기전 마지막 커밋

---
+++ InvaderAnotherWorld/InvaderAnotherWorld/Assets/UnitTestFolder/JinYoung/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazerd;
+    public int hazerdCount;
+    public Vector3 spawnValue;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazerdCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazerd, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
---
+++ InvaderAnotherWorld/InvaderAnotherWorld/Assets/UnitTestFolder/JinYoung/Scripts/GridMisslie.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMisslie : MonoBehaviour {
+    public Transform target;
+    public float speed = 15f;
+    public float rotateSpeed = 5;
+    //bool ifTarget = false;
+    Rigidbody rb;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    //Update is called once per frame
+    void FixedUpdate () {
+        //if(!ifTarget)
+        //{
+        //    rb.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        //    SetTarget();
+        //}
+        //else if (ifTarget)
+        //{
+        //    Vector3 direction = target.position - rb.position;
+        //    //direction.Normalize();
+        //    Quaternion q = Quaternion.LookRotation(direction);
+        //    Quaternion s = Quaternion.Slerp(transform.rotation, q, rotateSpeed * Time.deltaTime);
+        //    rb.rotation = s;
+        //    rb.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+
+        //}
+        
+        if (target!=null)
+        {
+            RuningToEnemy(target);
+        }
+        else
+        {
+            SetTarget();
+            //Debug.Log("settarget "+rb);
+            rb.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        }
+        //Debug.Log(target);
+        
+    }
+    void RuningToEnemy(Transform target)
+    {
+        Vector3 direction = target.position - rb.position;
+        Quaternion q = Quaternion.LookRotation(direction);
+        Quaternion s = Quaternion.Slerp(transform.rotation, q, rotateSpeed * Time.deltaTime);
+        rb.rotation = s;
+        rb.transform.Translate(new Vector3(0, 0, 1) * speed * Time.deltaTime);
+        
+    }
+    public void SetTarget()
+    {
+        if (GameObject.FindGameObjectWithTag("Enemy") != null)
+        {
+            GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+            float distance = Mathf.Infinity;
+            //for돌리고 if로 다음배열로 넘김//
+            foreach (GameObject enemy in enemys)
+            {
+                Vector3 diff = enemy.transform.position - rb.position;
+                float curDistance = diff.sqrMagnitude;
+
+                if (curDistance < distance)
+                {
+                    target = enemy.transform;
+                    distance = curDistance;
+                }
+            }
+        }
+    }
+}
