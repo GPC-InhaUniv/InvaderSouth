6f15c4f24bfb01f4432bc3e8c7d602947fb03313
정진영 <qufqlaud@gmail.com>
2018-06-27 18:05:33 +0900

    전문가그룹용테스트

---
+++ Jung_jinyoung/0627_GroupTest/Assets/Scripts/AudioTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour {
+    [SerializeField]
+    AudioClip audioClip;
+    [SerializeField]
+    AudioSource audioSource;
+    public static AudioTest Audio;
+    private void Start()
+    {
+        if (AudioTest.Audio == null)
+            AudioTest.Audio = this;
+        
+        audioSource = gameObject.AddComponent<AudioSource>();
+        //audioClip = GetComponent<AudioClip>();
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
---
+++ Jung_jinyoung/0627_GroupTest/Assets/Scripts/PlayAudioOneShotBtn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOneShotBtn : MonoBehaviour {
+
+	public void OnCrickButton()
+    {
+        Debug.Log("oncrick");
+        AudioTest.Audio.PlayAudio();
+    }
+}
